84ccb71c2792d931b41d4bf3d22bcc44f4628506
Denidu <denidu.20210404@iit.ac.lk>
2023-02-25 10:27:51 +0530

    Created a method for colour correction of a image

--- ColorCorrection.py
+++ ColorCorrection.py
@@ -9,12 +9,35 @@ from Utils import Modify,LoadImage
 
 class Main:
 
+
+    @staticmethod
     def correctImage(get_path:str,
                      degree_of_protanopia : float = 1.0,
                      degree_of_deutranopia : float = 1.0,
                      return_type:str = 'save',
                      save:str = None):
 
-     rgb_image = Utils.process_RGB(get_path) 
-     print
+     rgb_image = LoadImage.process_RGB(get_path)
+
+     modify = Modify.colour_correction_matrix(degree_of_deutranopia = degree_of_deutranopia,
+                                              degree_of_protanopia = degree_of_protanopia) 
+     
+     corrected_image = np.uint8(np.dot(rgb_image,modify)*255)
+
+     if return_type == 'PIL':
+        return Image.fromarray(corrected_image)
+     
+     if return_type == 'np':
+        return corrected_image
+
+     if return_type == 'save':
+        assert save is not None , 'Save path is not provided for image!'
+        cv2.imwrite(corrected_image,save)
+        return
+     
+    
+    
+
+
+    
                
