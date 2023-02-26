6d9bebc1e869fb8504722378e8afdf1f78db24d4
Denidu <denidu.20210404@iit.ac.lk>
2023-02-26 21:59:25 +0530

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -9,21 +9,21 @@ class Modify:
 
         return np.array([[17.8824, 43.5161, 4.11935],
                          [3.45565, 27.1554, 3.86714],
-                         [0.0299566, 0.184309, 1.46709]])
+                         [0.0299566, 0.184309, 1.46709]]).T
 
     @staticmethod
     def LMS_TO_RGB() -> np.ndarray:
 
         return np.array([[0.0809, -0.1305, 0.1167],
                          [-0.0102, 0.0540, -0.1136],
-                         [-0.0004, -0.0041, 0.6935]])
+                         [-0.0004, -0.0041, 0.6935]]).T
 
     @staticmethod
     def colour_correction_matrix(degree_of_protanopia, degree_of_deutranopia) -> np.ndarray:
 
         return np.array([[1 - degree_of_deutranopia/2, degree_of_deutranopia/2, 0],
                          [degree_of_protanopia/2, 1 - degree_of_protanopia/2, 0],
-                         [degree_of_protanopia/4, degree_of_deutranopia/4, 1 - (degree_of_protanopia + degree_of_deutranopia)/4]])
+                         [degree_of_protanopia/4, degree_of_deutranopia/4, 1 - (degree_of_protanopia + degree_of_deutranopia)/4]]).T
 
 class LoadImage:
 
