e8624358923b7398894e0d0f4b21635b69d9fdc0
Denidu <denidu.20210404@iit.ac.lk>
2023-03-04 21:36:24 +0530

    Update colourcorrection.py

--- ColorCorrection.py
+++ ColorCorrection.py
@@ -9,7 +9,6 @@ from Utils import Modify,LoadImage
 
 class Main:
 
-
     @staticmethod
     def correctImage(get_path:str,
                      degree_of_protanopia : float = 1.0,
@@ -25,16 +24,18 @@ class Main:
      
      corrected_image = np.uint8(np.dot(rgb_image,modify)*255)
 
-     if return_type == 'PIL':
-        return Image.fromarray(corrected_image)
-     
-     if return_type == 'np':
-        return corrected_image
-
      if return_type == 'save':
         assert save is not None , 'Save path is not provided for image!'
         cv2.imwrite(corrected_image,save)
         return
+     
+     if return_type == 'np':
+        return corrected_image
+     
+     if return_type == 'PIL':
+        return Image.fromarray(corrected_image)
+
+    
   
 def parse_args():
    parser= argparse.ArgumentParser(description = 'Colour Correct Images for Colour-Blindness')
@@ -50,33 +51,38 @@ def parse_args():
    
    args = parser.parse_args()
 
-   return args
+   return args   
+
 
 def main():
-   args = parse_args()
+    args = parse_args()
 
-   get_path = args.input
-   name_of_image = get_path.split('/')[-1]
-   image_output_path = args.output
+    get_path = args.input
+    if get_path is None:
+        print('Input image path not provided!')
+        return
 
-   assert os.path.isdir(image_output_path),'Output path should be a Directory'
+    get_path = args.input
+    name_of_image = get_path.split('/')[-1]
+    image_output_path = args.output
 
-   run_all = False
-   if args.run_all:
-      run_all = True
+    assert os.path.isdir(image_output_path),'Output path should be a Directory'
 
+    run_all = False
+    if args.run_all:
+        run_all = True
 
-      if args.colours_correct or run_all:
-         Main.correctImage(get_path= get_path,
-                           return_type='save',
-                           save = '{}/{}_{}'.format(image_output_path,'colours_correct', name_of_image),
-                           degree_of_protanopia=args.degree_of_protonopia,
-                           degree_of_deutranopia=args.degree_of_deutranopia)
-         
-      print('Completed running! Check output Image in {}'.format(image_output_path))   
+        if args.colours_correct or run_all:
+            Main.correctImage(get_path= get_path,
+                               return_type='save',
+                               save = '{}/{}_{}'.format(image_output_path,'colours_correct', name_of_image),
+                               degree_of_protanopia=args.degree_of_protonopia,
+                               degree_of_deutranopia=args.degree_of_deutranopia)
 
+    print('Completed running! Check output Image in {}'.format(image_output_path))
 
 if __name__== '__main__':
    main()  
+
 
 
