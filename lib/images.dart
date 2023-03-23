dfc9aa8ec77ea199a566e83fd8f6b93b93751ed8
zaidh-01 <124909116+zaidh-01@users.noreply.github.com>
2023-03-23 10:52:09 +0530

    up

--- lib/images.dart
+++ lib/images.dart
@@ -7,7 +7,7 @@ sendPhoto(File photo) async {
   String base64Photo = base64Encode(photo.readAsBytesSync());
 
   // Send an HTTP request to the Flask back-end
-  String url = 'http://192.168.43.121:8000/api/upload';
+  String url = 'http://192.168.216.78:8000/api/upload';
   Map<String, String> headers = {'Content-Type': 'application/json'};
   Map<String, dynamic> body = {'photo': base64Photo, 'option': "Protanopia"};
   http.Response response =
