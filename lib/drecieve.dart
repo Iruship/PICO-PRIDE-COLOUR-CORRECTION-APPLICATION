509b7c8009cd50ce8121334d8132eeccfdf556db
zaidh-01 <124909116+zaidh-01@users.noreply.github.com>
2023-03-24 13:16:05 +0530

    update

--- lib/drecieve.dart
+++ lib/drecieve.dart
@@ -13,7 +13,7 @@ class _ImagedScreenState extends State<ImagedScreen> {
 
   Future<void> getImage() async {
     var response =
-        await http.get(Uri.parse('http://192.168.216.78:8000/api/recieve'));
+        await http.get(Uri.parse('http://172.27.18.107:8000/api/recieve'));
     if (response.statusCode == 200) {
       setState(() {
         imageUrl = response.bodyBytes;
