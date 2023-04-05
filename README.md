fff6efcc0c537804c76f97bc8b32a9a4a5d58e78
zaidh-01 <124909116+zaidh-01@users.noreply.github.com>
2023-04-05 15:44:30 +0530

    Updated One

---
+++ README.md
@@ -0,0 +1,20 @@
+# PICO PRIDE COLOUR CORRECTION APPLICATION
+
+ Team Members
+Irushi Perera -  w18674138 
+Zaidh Ishan - w18388329 
+Denidu Gamage - w18675603 
+Mandira De Silva - w18677045 
+Sathini Jayasuriya - w1867466
+
+The ability to see clearly is a necessity for every human being in the world. Some people
+have colour blindness, and in some situations, these individuals may not even know that they have
+the condition. Most people who are colour blind are able to see clearly, but they cannot identify
+red, green, and blue light clearly. Very rarely there are some people who cannot see colour at all.
+Colour blind people face many difficulties in daily life that ordinary people do not notice.
+Nowadays there are many online simulators, computer monitors and software to assist the colour
+blind as technology advances day by day. However real-time colour correction is still very
+defective. Therefore, team PICO PRIDE aims to develop a colour correcting app that translates
+the colours into the colours that the colour-blind person can see which is equal to the visual
+representation of the original image in real-time. The concentration of this research is employing
+the most efficient technologies to develop this colour correcting application.
---
+++ lib/receive.dart
@@ -0,0 +1,61 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ImageScreen extends StatefulWidget {
+  @override
+  _ImageScreenState createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  Uint8List imageUrl;
+
+  Future<void> getImage() async {
+    var response =
+        await http.get(Uri.parse('http://192.168.8.135:8000/api/recieve'));
+    if (response.statusCode == 200) {
+      setState(() {
+        imageUrl = response.bodyBytes;
+      });
+    } else {
+      throw Exception('Failed to load image');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 43, 43, 44),
+      appBar: AppBar(
+        elevation: 0,
+        // ignore: deprecated_member_use
+        brightness: Brightness.light,
+        backgroundColor: Color.fromARGB(255, 43, 43, 44),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+        ),
+        title: Text("Corrected Protonopia Image"),
+      ),
+      body: imageUrl == null
+          ? Center(child: CircularProgressIndicator())
+          : Image.memory(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
+    );
+  }
+}
